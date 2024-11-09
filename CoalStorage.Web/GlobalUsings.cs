b2ed6b305f9d5373df025d852aa59a0055bc1164
viperr familia <easy.timurwin@gmail.com>
2024-11-09 03:48:13 +0300

    #2

--- CoalStorage.Web/GlobalUsings.cs
+++ CoalStorage.Web/GlobalUsings.cs
@@ -1,5 +1,4 @@
-﻿global using CoalStorage.Web.Services;
-global using CoalStorage.Web.Routes;
+﻿global using CoalStorage.Web.Routes;
 global using Microsoft.EntityFrameworkCore;
 global using CoalStorage.Inftastructure.Data;
 
--- CoalStorage.Web/Program.cs
+++ CoalStorage.Web/Program.cs
@@ -1,22 +1,22 @@
+using CoalStorage.Inftastructure;
 using CoalStorage.Inftastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.addServices();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     
 }
+await app.InitialiseDatabaseAsync();
 
 app.UseOpenApi();
 app.UseSwaggerUi();
@@ -36,5 +36,7 @@ app.MapRazorPages();
 
 app.AddRoutes();
 
-
+app.Map("/", () => Results.Redirect("/swagger"));
 app.Run();
+
+public partial class Program { }
