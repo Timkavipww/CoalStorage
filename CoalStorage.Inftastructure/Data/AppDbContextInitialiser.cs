651a30a98892385e7db98a6cb85f630e0a15679a
viperr familia <easy.timurwin@gmail.com>
2024-11-09 03:48:13 +0300

    #1

---
+++ CoalStorage.Inftastructure/Data/AppDbContextInitialiser.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CoalStorage.Inftastructure.Data;
+
+public static class InitialiserExtensions
+{
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var initialiser = scope.ServiceProvider.GetRequiredService<AppDbContextInitialiser>();
+
+        await initialiser.InitialiseAsync();
+
+    }
+}
+
+    public class AppDbContextInitialiser
+    {
+        private readonly ILogger<AppDbContextInitialiser> _logger;
+        private readonly AppDbContext _context;
+
+        public AppDbContextInitialiser(ILogger<AppDbContextInitialiser> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task InitialiseAsync()
+        {
+            try
+            {
+                await _context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while initialising the database.");
+                throw;
+            }
+        }
+    }
---
+++ CoalStorage.Inftastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using CoalStorage.Core.Interfaces;
+using CoalStorage.Inftastructure.Data;
+using CoalStorage.Inftastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoalStorage.Inftastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionstring = configuration.GetConnectionString("DefaultConnection");
+
+        //Guard.Against.Null(connectionString, message: "Connection string 'DefaultConnection' not found.");
+        services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionstring));
+        services.AddOpenApiDocument(document => document.DocumentName = "a");
+        services.AddSwaggerDocument(document => document.DocumentName = "b");
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        services.AddScoped<AppDbContextInitialiser>();
+        return services;
+    }
+}
